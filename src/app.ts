b6c3182bddb487b8616f16000f296c8b242b852e
Luiz Andre Nunes da Silva <luizandrenunesdasilva@Luizs-MacBook-Pro.local>
2020-05-21 18:54:29 -0300

    Feature: user create

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import 'reflect-metadata';
 import './database';
-
 import express from 'express';
 import cors from 'cors';
 
--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -1,8 +1,40 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+
+import { hash } from 'bcryptjs';
+import User from '../models/User';
 
 class UserController {
   public async store(request: Request, response: Response): Promise<Response> {
-    return response.json({ ok: true });
+    const { name, email, cpf, password } = request.body;
+
+    const userRepository = getRepository(User);
+
+    const emailExists = await userRepository.findOne({
+      where: { email },
+    });
+
+    const cpfExists = await userRepository.findOne({
+      where: { cpf },
+    });
+
+    if (emailExists || cpfExists) {
+      return response.json({ error: 'Email or CPF already exists' });
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const createUsers = await userRepository.create({
+      name,
+      email,
+      cpf,
+      password: hashedPassword,
+      provider: false,
+    });
+
+    await userRepository.save(createUsers);
+
+    return response.json(createUsers);
   }
 }
 
