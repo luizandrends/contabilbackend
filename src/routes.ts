1a3726f558c04fb6dee2f50ebe354d4ee230cbdd
Luiz Andre Nunes da Silva <luizandrenunesdasilva@Luizs-MacBook-Pro.local>
2020-05-21 20:53:25 -0300

    Feature: Organizing the routes file

--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,16 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import AuthMiddleware from './app/middleware/auth';
+
 const routes = Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(AuthMiddleware);
+
 routes.get('/users', UserController.list);
 routes.get('/users/:id', UserController.get);
-
-routes.post('/sessions', SessionController.store);
 
 export default routes;
