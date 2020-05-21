e1188d7314962f8ef9cde731a0c64991ff81bcf7
Luiz Andre Nunes da Silva <luizandrenunesdasilva@Luizs-MacBook-Pro.local>
2020-05-21 20:51:25 -0300

    Feature: auth middleware

---
+++ src/app/middleware/auth.ts
@@ -0,0 +1,38 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import authConfig from '../../config/auth';
+
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Response | void {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    return response.status(401).json({ error: 'Token is missing' });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = verify(token, authConfig.jwt.secret);
+
+    const { sub } = decoded as TokenPayload;
+
+    request.user = {
+      id: sub,
+    };
+
+    return next();
+  } catch (err) {
+    return response.json({ error: 'Invalid JWT token' });
+  }
+}
