0eb561f860c82dfc653bc5e42608680b4b3addd7
Luiz Andre Nunes da Silva <luizandrenunesdasilva@Luizs-MacBook-Pro.local>
2020-05-21 21:13:43 -0300

    Chore: migration of files

---
+++ src/database/migrations/1590106408785-CreateFilesTable.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateFilesTable1590106408785 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
