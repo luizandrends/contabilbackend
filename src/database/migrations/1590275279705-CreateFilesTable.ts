3cfccc31893d27d1caffcaf3e0358f47e2a5351e
Luiz Andre Nunes da Silva <luizandrenunesdasilva@Luizs-MacBook-Pro.local>
2020-05-23 20:42:55 -0300

    Chore: Files migrations

---
+++ src/database/migrations/1590275279705-CreateFilesTable.ts
@@ -0,0 +1,73 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class CreateFilesTable1590274553923
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'files',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'path',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'deleted_at',
+            type: 'timestamp',
+            default: null,
+            isNullable: true,
+          },
+        ],
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      'files',
+      new TableForeignKey({
+        name: 'UserFieldToFiles',
+        columnNames: ['user_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('files', 'UserFieldToFiles');
+
+    await queryRunner.dropColumn('user_id', 'files');
+  }
+}
