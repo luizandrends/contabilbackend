2312c36ed88a4765d952f62cdd4c52d4e4bfa7a0
Luiz Andre Nunes da Silva <luizandrenunesdasilva@Luizs-MacBook-Pro.local>
2020-05-23 20:59:56 -0300

    Fix: Fix on the foreign key of the user_id column

--- src/app/models/UserPasswordRecovery.ts
+++ src/app/models/UserPasswordRecovery.ts
@@ -6,7 +6,11 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   DeleteDateColumn,
+  ManyToOne,
+  JoinColumn,
 } from 'typeorm';
+
+import User from './User';
 
 @Entity('user_password_recovery')
 class UserPasswordRecovery {
@@ -19,6 +23,10 @@ class UserPasswordRecovery {
 
   @Column()
   user_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
 
   @CreateDateColumn()
   created_at: Date;
