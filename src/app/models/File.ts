65b136ba43577f50277c313dc2bed2c3ddf16ce2
Luiz Andre Nunes da Silva <luizandrenunesdasilva@Luizs-MacBook-Pro.local>
2020-05-23 21:06:15 -0300

    Feature: File model

---
+++ src/app/models/File.ts
@@ -0,0 +1,41 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import User from './User';
+
+@Entity('files')
+class File {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  path: string;
+
+  @Column()
+  user_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
+}
+
+export default File;
