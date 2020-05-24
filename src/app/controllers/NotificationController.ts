3b7c8b2d3ced8cb3361516e67e91b5fdc4568be0
Luiz Andre Nunes da Silva <luizandrenunesdasilva@Luizs-MacBook-Pro.local>
2020-05-23 23:59:41 -0300

    Feature: Only providers can list and update notifi

--- src/app/controllers/NotificationController.ts
+++ src/app/controllers/NotificationController.ts
@@ -1,15 +1,40 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
 
 import Notification from '../schemas/Notification';
 
+import User from '../models/User';
+
 class NotificationController {
   public async list(request: Request, response: Response): Promise<Response> {
+    const userRepository = getRepository(User);
+    const checkUserProvider = await userRepository.findOne({
+      where: { id: request.user.id, provider: true },
+    });
+
+    if (!checkUserProvider) {
+      return response
+        .status(401)
+        .json({ error: 'Only providers can get notifications' });
+    }
+
     const findNotifications = await Notification.find();
 
     return response.json(findNotifications);
   }
 
   public async update(request: Request, response: Response): Promise<Response> {
+    const userRepository = getRepository(User);
+    const checkUserProvider = await userRepository.findOne({
+      where: { id: request.user.id, provider: true },
+    });
+
+    if (!checkUserProvider) {
+      return response
+        .status(401)
+        .json({ error: 'Only providers can update notifications' });
+    }
+
     const { id } = request.params;
 
     const updateNotification = await Notification.findByIdAndUpdate(
